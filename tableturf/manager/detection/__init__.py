40495f0ecc6b878a08d5264c82ecd61da9e5867f
Lyew Marven <fga401@outlook.com>
2022-10-22 20:46:16 +0800

    rename

--- tableturf/manager/detection/__init__.py
+++ tableturf/manager/detection/__init__.py
@@ -1,3 +1,3 @@
-from tableturf.manager.detection.battle import hands, hands_cursor
+from tableturf.manager.detection.card import hands, hands_cursor
 from tableturf.manager.detection.deck import deck_cursor
 from tableturf.manager.detection.redraw import redraw_cursor
---
+++ tests/test_tableturf/test_detection/test_card.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+from capture import FileLoader
+from tableturf.manager import detection
+
+path = os.path.join(os.path.realpath(__file__), '..', '..', '..', '..', 'temp')
+
+
+@unittest.skipIf(not os.path.exists(path), 'images not existed')
+class TestCard(unittest.TestCase):
+    def test_hands_cursor(self):
+        capture = FileLoader(path=os.path.join(path, 'stage2'))
+        for _ in range(10):
+            detection.hands_cursor(capture.capture(), True)
+
+    def test_hands_in_battle(self):
+        capture = FileLoader(path=os.path.join(path, 'stage2'))
+        for _ in range(10):
+            detection.hands(capture.capture(), None, True)
+
+    def test_hands_in_redraw(self):
+        capture = FileLoader(path=os.path.join(path, 'redraw'))
+        for _ in range(10):
+            detection.hands(capture.capture(), None, True)
